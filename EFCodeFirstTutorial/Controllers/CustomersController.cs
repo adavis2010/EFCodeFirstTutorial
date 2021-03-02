407de6f6e797dbe30f4b9ccc552d1b9352385848
max-student <max-student@maxtrain.com>
2021-03-01 20:22:55 -0500

    Added Methods to CC

---
+++ EFCodeFirstTutorial/Controllers/CustomersController.cs
@@ -0,0 +1,86 @@
+﻿
+using EFCodeFirstTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstTutorial.Controllers {
+    public class CustomersController {
+
+        private readonly AppDbContext _context;
+
+        //default constructor
+        public CustomersController() {
+            _context = new AppDbContext();
+        }
+
+        //Get All
+        public async Task<IEnumerable<Customer>> GetAll() {
+            return await _context.Customers.ToListAsync();
+        }
+
+        //Get by Primary Key
+        public async Task<Customer> GetByPk(int id) {
+            return await _context.Customers.FindAsync(id);
+        }
+
+        //Insert /Create
+        public async Task<Customer> Create(Customer customer) {
+            if (customer == null) {
+                throw new Exception("Input cannot be null");
+            }
+
+            if (customer.Id != 0) {
+                throw new Exception("Input must have Id set to zero");
+            }
+            _context.Customers.Add(customer);
+            var rowsAffected = await _context.SaveChangesAsync();
+            if (rowsAffected != 1) {
+                throw new Exception("Create Failed!");
+            }
+            return customer;
+
+        }
+
+        //Update/ Change
+        public async Task Change(Customer customer) {
+            if (customer == null) {
+                throw new Exception("Input cannot be null");
+            }
+            if (customer.Id == 0) {
+                throw new Exception("Input must have Id greater than zero");
+            }
+            _context.Entry(customer).State = EntityState.Modified;
+            var rowsAffected = await _context.SaveChangesAsync();
+            if (rowsAffected != 1) {
+                throw new Exception("Change Failed!");
+            }
+
+            return;
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
---
+++ EFCodeFirstTutorial/Controllers/OrderlinesController.cs
@@ -0,0 +1,10 @@
+﻿using EFCodeFirstTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstTutorial.Controllers {
+    class OrderlinesController {
+    }
+}
